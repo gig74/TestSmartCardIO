74d0fcbb03a5f146bff05ddf6ecf270073824483
gig74 <gig74_tmp@mail.ru>
2025-05-06 16:45:09 +0500

    00 - in progress 01

--- src/main/java/com/test/smartcard/Blog.java
+++ src/main/java/com/test/smartcard/Blog.java
@@ -25,10 +25,11 @@ public class Blog {
             System.out.printf("  Card ATR: %s%n", aa);
 
             var channel = card.getBasicChannel();
-            byte[] command = {(byte)0xFF, (byte)0xCA, (byte)0x00, (byte)0x00, (byte)0x00};
-            ResponseAPDU answer = channel.transmit(new CommandAPDU(command));
-            String uid = bytesToHex(answer.getBytes());
-            System.out.printf("Card UID: %s%n", uid);
+            int[] command = {0xFF, 0xCA, 0x00, 0x00, 0x00}; // Получение UID команда APDU
+            ResponseAPDU answer = channel.transmit(new CommandAPDU(intArrayToByteArray(command)));
+            int returnCode = answer.getSW(); // Код возврата APDU команды (0x9000 - нормальное завершение)
+            byte[] uid = answer.getData(); // Данные, возвращаемые по APDU команде (без кода возврата)
+            System.out.printf("Card UID: %s%n", bytesToHex(uid));
 
             card.disconnect(true);
 
@@ -37,6 +38,10 @@ public class Blog {
             System.out.println("Problem: " + e.toString());
         }
     }
+    //  Card ATR: 3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 36 00 00 00 00 5D - карта тройка
+    //  Card ATR: 3B 8B 80 01 00 31 C0 64 08 44 03 77 00 90 00 37 - банковская карта
+    //  Card ATR: 3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 01 00 00 00 00 6A - "пустая" карта Mifare classic 1 K
+    //  Card ATR: 3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 03 00 00 00 00 68 - карта на NXP NTag213
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
     public static String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
@@ -49,8 +54,11 @@ public class Blog {
         s = s.replaceAll("..", "$0 ");
         return s;
     }
-    //  Card ATR: 3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 36 00 00 00 00 5D - карта тройка
-    //  Card ATR: 3B 8B 80 01 00 31 C0 64 08 44 03 77 00 90 00 37 - банковская карта
-    //  Card ATR: 3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 01 00 00 00 00 6A - "пустая" карта Mifare classic 1 K
-    //  Card ATR: 3B 8F 80 01 80 4F 0C A0 00 00 03 06 03 00 03 00 00 00 00 68 - карта на NXP NTag213
+    public static byte[] intArrayToByteArray(int[] intArray) {
+        byte[] byteArray = new byte[intArray.length];
+        for (int j = 0; j < intArray.length; j++) {
+            byteArray[j] = (byte) intArray[j];
+        }
+        return byteArray;
+    }
 }
